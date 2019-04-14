1e01bd8adaae9d41c21ca610ea93ae4c47e6843d
Micaela Signorelli <m.signorelli@gmail.com>
2019-04-14 17:04:23 +0100

    check .env file exists before loading

--- manage.py
+++ manage.py
@@ -4,7 +4,9 @@ import sys
 import dotenv
 
 if __name__ == "__main__":
-    dotenv.read_dotenv()
+    if os.path.isfile(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')):
+        dotenv.read_dotenv()
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "UnicornTicketSystem.settings")
     try:
         from django.core.management import execute_from_command_line
