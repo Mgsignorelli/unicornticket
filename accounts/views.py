833605b737245f9c13450cc6a4183285f1429cf2
Micaela Signorelli <m.signorelli@gmail.com>
2019-04-27 14:49:01 +0100

    adds logout view to accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     """Returns index page"""
     return render(request,  'index.html')
+
+@login_required
+def logout(request):
+    """Logs out a user"""
+    auth.logout(request)
+    messages.success(request, "You've successfully logged out!")
+    return redirect(reverse('index'))
 
