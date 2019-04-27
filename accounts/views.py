039ea0c09447a08737a9367f4cf2c11cc04fa94a
Micaela Signorelli <m.signorelli@gmail.com>
2019-04-27 14:51:33 +0100

    adds login view to accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, reverse
+from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
+from accounts.forms import UserLoginForm
 
 def index(request):
     """Returns index page"""
@@ -12,3 +14,23 @@ def logout(request):
     messages.success(request, "You've successfully logged out!")
     return redirect(reverse('index'))
 
+def login(request):
+    """Returns login page"""
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+    if request.method == "POST":
+        login_form = UserLoginForm(request.POST)
+
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'],
+                                    password=request.POST['password'])
+            messages.success(request, "You have successfully logged in!")
+
+            if user:
+                auth.login(user=user, request=request)
+                return redirect(reverse('index'))
+            else:
+                login_form.add_error(None, "Your username or password is incorrect")
+    else:
+        login_form = UserLoginForm()
+    return render(request, 'login.html', {'login_form': login_form})
