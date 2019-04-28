2937ddad534c3b570d46e8458dbcdce97220bf96
Micaela Signorelli <m.signorelli@gmail.com>
2019-04-28 17:28:49 +0100

    take screenshots on failed steps

--- features/environment.py
+++ features/environment.py
@@ -1,10 +1,12 @@
+import os
 from behave import *
+from behave.model_core import Status
 from splinter import Browser
 
 
 @fixture
 def web_browser(context):
-    context.browser = Browser()
+    context.browser = Browser('chrome', headless=True)
     yield context.browser
     context.browser.quit()
 
@@ -21,3 +23,10 @@ def clean_screenshots(context):
 def before_all(context):
     use_fixture(clean_screenshots, context)
     use_fixture(web_browser, context)
+
+
+def after_step(context, current_step):
+    if current_step.status == Status.failed:
+        dir_name = os.path.dirname(os.path.realpath(__file__)) + '/screenshots/'
+        file_name = current_step.keyword + ' ' + current_step.name
+        context.browser.screenshot(dir_name + file_name + '.png')
