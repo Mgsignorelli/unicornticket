6b92cc085313127cb0add6c8edc163486671d25d
Micaela Signorelli <m.signorelli@gmail.com>
2019-07-14 21:37:17 +0100

    add ability to log work done on a feature or bug

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from tickets.models import Feature, Bug
+
+
+def add_work(modeladmin, request, queryset):
+    for ticket in queryset:
+        if isinstance(ticket, Bug):
+            ticket.bugwork_set.create()
+
+        if isinstance(ticket, Feature):
+            ticket.featurework_set.create()
+
+
+add_work.short_description = "Mark as worked on"
+
+
+@admin.register(Feature, Bug)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']
+    ordering = ['title']
+    actions = [add_work]
