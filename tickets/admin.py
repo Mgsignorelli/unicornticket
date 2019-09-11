4a7e3aac7588b3a4aa28fcb4156dccea78e6d33c
Micaela Signorelli <m.signorelli@gmail.com>
2019-09-11 22:06:36 +0100

    allow admin to delete comments in admin panel

--- tickets/admin.py
+++ tickets/admin.py
@@ -23,8 +23,28 @@ def delete_ticket(modeladmin, request, queryset):
 delete_ticket.short_description = "Delete ticket"
 
 
+def delete_comment(modeladmin, request, queryset):
+    for comment in queryset:
+        if isinstance(comment, Bug):
+            comment.delete()
+
+        if isinstance(comment, Feature):
+            comment.delete()
+
+
+delete_comment.short_description = "Delete comment"
+
+
+# Permits Admin to see Feature and Bug Tickets and delete them in Admin panel
 @admin.register(Feature, Bug)
 class TicketAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
     ordering = ['title']
     actions = [add_work, delete_ticket]
+
+
+# Permits Admin to see Feature and Bug Comments and delete them in Admin panel
+@admin.register(FeatureComment, BugComment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['content', 'created_date']
+    ordering = ['created_date']
