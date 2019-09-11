96bf33ba08bc4fd9f1839ad1ae373c8de24016fc
Micaela Signorelli <m.signorelli@gmail.com>
2019-09-11 22:06:13 +0100

    allow admin to delete tickets in admin panel

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from tickets.models import Feature, Bug
+from tickets.models import Feature, Bug, Comment, FeatureComment, BugComment
 
 
 def add_work(modeladmin, request, queryset):
@@ -15,8 +15,16 @@ def add_work(modeladmin, request, queryset):
 add_work.short_description = "Mark as worked on"
 
 
+def delete_ticket(modeladmin, request, queryset):
+    for ticket in queryset:
+        ticket.delete()
+
+
+delete_ticket.short_description = "Delete ticket"
+
+
 @admin.register(Feature, Bug)
 class TicketAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
     ordering = ['title']
-    actions = [add_work]
+    actions = [add_work, delete_ticket]
